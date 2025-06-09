fcb8f0635ccfe57b7a79dea5077615a0490c6708
malani86 <134076665+malani86@users.noreply.github.com>
2025-06-10 01:00:26 +0200

    Add files via upload

---
+++ install_packages.py
@@ -0,0 +1,30 @@
+import subprocess
+import sys
+
+required_packages = [
+    "numpy",
+    "opencv-python",
+    "pillow",
+    "torch>=1.7",
+    "torchvision",
+    "albumentations",
+    "matplotlib",
+    "seaborn",
+    "scikit-image",
+    "scikit-learn",
+    "pandas",
+    "tqdm",
+    "xlsxwriter"
+]
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+if __name__ == "__main__":
+    for package in required_packages:
+        try:
+            print(f"Installing: {package}")
+            install(package)
+        except Exception as e:
+            print(f"Failed to install {package}: {e}")
+
